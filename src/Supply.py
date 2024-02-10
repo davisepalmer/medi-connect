f8dcaf9cbbe49f8550d0a86308ab075b93bd63e6
Davis Palmer <davisepalmer@gmail.com>
2024-02-09 21:32:57 -0600

    supply obj

---
+++ src/Supply.py
@@ -0,0 +1,25 @@
+
+class Supply:
+    def __init__(self, isNeeded, item = "", quantity=0, originHosp=""):
+        self.isNeeded = isNeeded 
+        self.item = item
+        self.quantity = quantity
+        self.originHosp = originHosp
+
+    ### DATA MEMBER EDITORS ###
+    def decQuantity(self, quantity, count=1):
+        self.quantity -= count
+    
+    def changeOriginHosp(self, originHosp):
+        self.originHosp = originHosp
+    
+    def changeIsNeeded(self, isNeeded):
+        self.isNeeded = isNeeded
+    
+    def deleteSupple(self):
+        self.isNeeded = False
+        self.quantity = -1
+        self.originHosp = ""
+    
+    
+
