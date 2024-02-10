5eeafd1d6bf31c8f11cb9e57fbfdf359043cea35
Leela Ravoori <lravoori@gmail.comsdlkjfsf>
2024-02-09 22:55:36 -0500

    hu

--- src/Diagnosis.py
+++ src/Diagnosis.py
@@ -1,5 +1,7 @@
 class Diagnosis:
     def __init__(self, photo, originHosp=""):
-        self.photo = photo
+        self.photo = -1
         self.originHosp = originHosp
-
+    
+    def deletePhoto(self):
+        self.photo = -1
