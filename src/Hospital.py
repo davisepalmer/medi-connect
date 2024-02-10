c418d453de108c2be715870892978f8ef1d4372b
Davis Palmer <davisepalmer@gmail.com>
2024-02-10 04:45:07 -0600

    hosp

--- src/Hospital.py
+++ src/Hospital.py
@@ -1,2 +1,38 @@
+from src.Supply import Supply
+from src.Diagnosis import Diagnosis
+
 class Hospital:
+    def __init__(self, hospName, beds = 0, supplies=[]):
+        self.hospName = hospName
+        self.beds = beds
+        self.supplies = supplies
     
+    ### DATA MEMBER EDITORS ###
+    def addSupply(self, supply):
+        self.supplies.append(supply)
+    
+    def changeBeds(self, count):
+        ```Change the number of beds in the hospital: should be negative to decrement```
+        self.beds += count
+
+    def makeSupply(self, isNeeded, item = "", quantity=0, originHosp="")
+        supply = Supply(isNeeded, item, quantity, originHosp)
+        self.addSupply(supply)
+    
+    def giveSupply(self, supply, destinationHosp): #i dont know if we need this functionality
+        supply.changeOriginHosp(destinationHosp)
+        destinationHosp.addSupply(supply)
+        self.supplies.remove(supply)
+    
+    def deleteSupply(self, supply):
+        supply.deleteSupple()
+        self.supplies.remove(supply)
+    
+    # SEND DIAGNOSIS
+    # etc etc
+
+    
+    
+    
+    
+     
