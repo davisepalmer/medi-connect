3af8919e8430be3529adfdda71c0e64a9e7f7b79
Davis Palmer <davisepalmer@gmail.com>
2024-02-10 16:46:14 -0500

    tex file + diag + index + setting

--- src/nonObjDiagnosis.py
+++ src/nonObjDiagnosis.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_pymongo import PyMongo
 from app import app, mongo
+
 @app.route('/diagnosis')
 def diagnosis():
     return render_template('diagnosis.html')
