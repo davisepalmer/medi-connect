f8a847afaa1f8f1f5b1c0a8a5d0c4022a04ff1f5
Leela Ravoori <lravoori@gmail.comsdlkjfsf>
2024-02-11 01:51:25 -0500

    decrementextra

--- src/decrement.py
+++ src/decrement.py
@@ -4,32 +4,51 @@ from flask_pymongo import PyMongo
 from app import app, mongo
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://new-user-2:qy5T1AM3I9oCN8tk@supplies.nyyixjh.mongodb.net/<database>?retryWrites=true&w=majority")
-db = client.get_default_database()
-giving = db['giving']
+
+
+app.config["MONGO_URI"] = "mongodb+srv://new-user-2:qy5T1AM3I9oCN8tk@supplies.nyyixjh.mongodb.net/<database>?retryWrites=true&w=majority"
+mongo = PyMongo(app)
 
 @app.route('/decrement', methods=['POST'])
 def decrement():
     data = request.json
     supply_name = data.get('supplyName')
     count = int(data.get('decrementCount'))
-    
+
     if supply_name and count > 0:
-        for i in mongo.db.giving:
-            if i["name"] == supply_name:
-                current_quantity = i["quantity"]
+        # Find the document with the specified supply name
+        supply = mongo.db.giving.find_one({'item': supply_name})
+        if supply:
+            current_quantity = supply.get('quantity')
+            new_quantity = current_quantity - count
+            if new_quantity >= 0:
+                # Update the quantity in the document
+                mongo.db.giving.update_one({'item': supply_name}, {'$set': {'quantity': new_quantity}})
+                return jsonify({'message': 'Claimed successfully'}), 200
             else:
-                return jsonify({'error': 'Supply not found'}), 404
-        new_quantity = current_quantity - count
-        if new_quantity < 0:
-            return jsonify({'message': 'Error: Not enough supply available'}), 400
+                return jsonify({'error': 'Error: Not enough supply available'}), 400
         else:
-            for i in mongo.db.giving:
-                if i["name"] == supply_name:
-                    i["quantity"] = new_quantity
-                    return jsonify({'message': 'Claimed successfully'}), 200
-    else: 
+            return jsonify({'error': 'Supply not found'}), 404
+    else:
         return jsonify({'error': 'Invalid data provided'}), 400
+    
+    # if supply_name and count > 0:
+    #     supply = mongo.db.giving.find_one
+    #     for i in mongo.db.giving:
+    #         if i["name"] == supply_name:
+    #             current_quantity = i["quantity"]
+    #         else:
+    #             return jsonify({'error': 'Supply not found'}), 404
+    #     new_quantity = current_quantity - count
+    #     if new_quantity < 0:
+    #         return jsonify({'message': 'Error: Not enough supply available'}), 400
+    #     else:
+    #         for i in mongo.db.giving:
+    #             if i["name"] == supply_name:
+    #                 i["quantity"] = new_quantity
+    #                 return jsonify({'message': 'Claimed successfully'}), 200
+    # else: 
+    #     return jsonify({'error': 'Invalid data provided'}), 400
 
 
 
